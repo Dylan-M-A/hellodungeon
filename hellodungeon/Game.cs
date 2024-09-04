4a89ff9ec8dca07ca2b2e60c60496b4019574ea4
Dylan-M-A <darmentorbusiness@gmail.com>
2024-09-04 15:25:26 -0500

    got rid of maze code

--- hellodungeon/Game.cs
+++ hellodungeon/Game.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Intrinsics;
 using System.Text;
@@ -244,84 +243,6 @@ namespace HelloDungeon
 
             Console.Clear();
             return inputRecieved;
-        }
-
-        private const char wallchar = '\u2588';
-        private const char mazechar = '\u0020';
-
-        static private bool[][] mazeLayout =
-        {
-        new[] {true, true, true, true, true, true, true, true},
-        new[] {true, true, true, true, true, true, true, true},
-        new[] {true, false, false, false, false, false, false, true},
-        new[] {true, false, true, true, true, true, false, true},
-        new[] {true, false, true, false, false, false, false, true},
-        new[] {true, false, true, false, true, true, true, true},
-        new[] {true, false, true, false, false, false, false, true},
-        new[] {true, false, true, true, true, true, false, true},
-        new[] {true, false, false, false, false, false, false, true},
-        new[] {true, true, true, true, true, true, true, true}
-    };
-
-        static void Main(string[] args)
-        {
-            Console.SetWindowSize(10, 10);
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-
-            Console.WriteLine("");
-
-            //Make maze
-            for (int i = 0; i < mazeLayout.Length; i++)
-            {
-                for (int j = 0; j < mazeLayout[i].Length; j++)
-                {
-                    Console.Write(mazeLayout[i][j] ? wallchar : mazechar);
-                }
-                Console.Write("\n");
-            }
-
-            Console.WriteLine("");
-            int col = 3;
-            int row = 6;
-
-            Console.SetCursorPosition(col, row);
-            Console.Write("*");
-            Console.SetCursorPosition(col, row);
-            while (true)
-            {
-                ConsoleKeyInfo info = Console.ReadKey(true);
-                if (info.Key == ConsoleKey.W && !mazeLayout[row - 2][col])
-                {
-                    Console.Write(" ");
-                    Debug.Print("W");
-                    row--;
-                }
-                if (info.Key == ConsoleKey.Z && !mazeLayout[row][col])
-                {
-                    Console.Write(" ");
-                    Debug.Print("Z");
-                    row++;
-                }
-
-                if (info.Key == ConsoleKey.A && !mazeLayout[row - 1][col - 1])
-                {
-                    Console.Write(" ");
-                    Debug.Print("A");
-                    col--;
-                }
-
-                if (info.Key == ConsoleKey.S && !mazeLayout[row - 1][col + 1])
-                {
-                    Console.Write(" ");
-                    Debug.Print("S");
-                    col++;
-                }
-
-                Console.SetCursorPosition(col, row);
-                Console.Write("*");
-                Console.SetCursorPosition(col, row);
-            }
         }
     }
 }
