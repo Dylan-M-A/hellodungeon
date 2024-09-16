f7f42d23239326b5653af1d7f36007b4a47ded94
Dylan-M-A <darmentorbusiness@gmail.com>
2024-09-16 14:32:51 -0500

    deleted maze and implemented boss battle

--- hellodungeon/Game.cs
+++ hellodungeon/Game.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime;
 using System.Runtime.Intrinsics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HelloDungeon
 {
     internal class Game
     {
-        struct Player
+        bool playerIsAlive = true;
+        int currentArea = 1;
+        bool gameOver = false;
+
+        struct Player1
         {
             public float health;
             public float stamina;
             public float mana;
+            public int strength;
+            public int speed;
+            public int defense;
             public int gold;
             public int silver;
             public int bronze;
@@ -22,9 +32,7 @@ namespace HelloDungeon
             public string race;
             public string weapon;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-            public Player(
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+            public Player1(
                 string name,
                 string role,
                 string race,
@@ -32,26 +40,60 @@ namespace HelloDungeon
                 float health,
                 float stamina,
                 float mana,
+                int strength,
+                int speed,
+                int defense,
                 int gold,
                 int silver,
                 int bronze)
             {
                 this.name = name;
-                this.name = race;
+                this.race = race;
                 this.weapon = weapon;
-                this.name = role;
+                this.role = role;
                 this.health = health;
                 this.stamina = stamina;
                 this.mana = mana;
+                this.strength = strength;
+                this.speed = speed;
+                this.defense = defense;
                 this.gold = gold;
                 this.silver = silver;
                 this.bronze = bronze;
             }
         }
-        public void Run()
+        struct DungeonMaster
+        {
+            public string name;
+            public float health;
+            public float stamina;
+            public float mana;
+            public int strength;
+            public int speed;
+            public int defense;
+
+            public DungeonMaster(
+                string name,
+                float health,
+                float stamina,
+                float mana,
+                int strength,
+                int speed,
+                int defense)
+            {
+                this.name = name;
+                this.health = health;
+                this.stamina = stamina;
+                this.mana = mana;
+                this.strength = strength;
+                this.speed = speed;
+                this.defense = defense;
+            }
+        }
+        Player1 mainCharacter = new Player1(name: "", role: "", race: "", weapon: "", 100, 100, 50f, 50, 25, 40, 3, 10, 7);
+        void Room1()
         {
             //declaring Player mainCharacter and the stats
-            Player mainCharacter = new Player(name: "", role: "", race: "", weapon: "", 10, 10, 5f, 3, 10, 7);
             Console.WriteLine("Hello, your name please");
 #pragma warning disable CS8601 // Possible null reference assignment.
             mainCharacter.name = Console.ReadLine();
@@ -78,11 +120,9 @@ namespace HelloDungeon
                 mainCharacter.role = "Brawler";
                 Console.WriteLine("Player role: " + mainCharacter.role);
                 Console.WriteLine("Stats: ");
-                Console.WriteLine("Strength: 5");
-                Console.WriteLine("Speed: 2");
-                Console.WriteLine("Agility: 3");
-                Console.WriteLine("Defense: 4");
-                Console.WriteLine("Intelligence: 3");
+                Console.WriteLine("Strength: " + mainCharacter.strength);
+                Console.WriteLine("Speed: " + mainCharacter.speed);
+                Console.WriteLine("Defense: " + mainCharacter.defense);
                 Console.WriteLine();
             }
             else if (input == 2)
@@ -104,39 +144,108 @@ namespace HelloDungeon
             Console.WriteLine("Gold: " + mainCharacter.gold);
             Console.WriteLine("Silver: " + mainCharacter.silver);
             Console.WriteLine("Bronze: " + mainCharacter.bronze);
-
-            //mainCharacter says name of weapon
-            Console.WriteLine();
-            Console.WriteLine("So what is your weapon you carry.");
-#pragma warning disable CS8601 // Possible null reference assignment.
-            mainCharacter.weapon = Console.ReadLine();
-#pragma warning restore CS8601 // Possible null reference assignment.
-            Console.WriteLine("Weapon: " + mainCharacter.weapon);
             Console.WriteLine();
             {
                 int v = GetInput("You have to choose out of two doors which one to go through.", "Left Door", "Right Door");
                 if (v == 1)
                 {
                     Console.WriteLine("Good Job You Died.");
+                    playerIsAlive = false;
                 }
                 else if (v == 2)
                 {
                     Console.WriteLine("You enter the right door.");
                     Console.WriteLine();
-                    Console.WriteLine("Great, you've made it to the next set of puzzles.");
-                    Console.WriteLine("You're next puzzle will be a riddle.");
+                    Console.WriteLine("Great, you've made it to the next area.");
+                }
+            }
+        }
+        void Room2()
+        {
+            int numberOfAttempts = 5;
+            string input = "";
+
+            for (int i = 0; i < numberOfAttempts; i--)
+            {
+                Console.Clear();
+
+                Console.WriteLine("You're next puzzle will be a riddle.");
+                Console.WriteLine("You will have " + numberOfAttempts + " attempts.");
+                Console.WriteLine();
+                Console.WriteLine("Man walks over, man walks under, in times of war he burns asunder", "Bridge");
+
+                int attemptsRemaining = numberOfAttempts + i;
+
+                Console.WriteLine("Attempts Remaining: " + attemptsRemaining);
+
+                Console.Write("> ");
+                input = Console.ReadLine();
+
+                if (input == "bridge")
+                {
+                    Console.WriteLine("Correct, you are smarter than I thought.");
+                    Console.WriteLine("Now onto the next challenge.");
                     Console.WriteLine();
-                    input = GetInput("Man walks over, man walks under, in times of war he burns asunder", "Bridge");
-                    if (input == 1)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Correct, you are smarter than I thought.");
-                        Console.WriteLine("Now onto the next challenge.");
-                        Console.WriteLine();
-                        Console.WriteLine("Your next puzzle will put you through a loop because its a maze.");
-                    }
+                    break;
                 }
 
+                Console.WriteLine("No, you are wrong of course.");
+                Console.ReadKey();
+                
+                if (attemptsRemaining <= 0)
+                {
+                    playerIsAlive = false;
+                    Console.WriteLine("Seems your not as smart as I thought, dumbass.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                }
+            }
+        }
+        void Room3()
+        {
+            Console.Clear();
+            Console.WriteLine("Now of course because this is a dungeon you are going to have to fight.");
+            Console.WriteLine("So you are goin to fight me. The Dungeon Master!");
+            DungeonMaster boss = new DungeonMaster("Dungeon Master", 200, 100, 100f, 45, 20, 30);
+
+            while (mainCharacter.health > 0 && boss.health > 0)
+            {
+                Console.WriteLine("_______________");
+                // monster1 attacks monster2
+                Console.WriteLine(boss.name + " Has taken " + Fight(mainCharacter, ref boss) + " damage!");
+
+                //monster2 attacks monster1
+                Console.WriteLine(mainCharacter.name + " Has taken " + Fight(boss, ref mainCharacter) + " damage!");
+
+                PrintStats(mainCharacter);
+                PrintStats(boss);
+            }
+            Console.ReadKey();
+        }
+        void Room4()
+        {
+            Console.Clear();
+            Console.WriteLine("You've beaten the Dungeon Master. Congratulaions.");
+        }
+       
+        void DisplayMainMenu()
+        {
+            //displays if you'd like to play again
+            int input = GetInput("Would you like to play again?", "Yes", "No");
+
+            //if player presses yes
+            if(input == 1)
+            {
+                //set currentArea to be start with reset stats
+                currentArea = 1;
+                gameOver = false;
+                playerIsAlive = true;
+            }
+            //if you quit
+            else if (input == 2)
+            {
+                gameOver = true;
             }
         }
         static int GetInput(string description, string option1, string option2)
@@ -216,7 +325,7 @@ namespace HelloDungeon
             string input = "";
             int inputRecieved = 0;
 
-            while (inputRecieved != 1)
+            while (inputRecieved != 1 && inputRecieved != 2)
             {
                 Console.WriteLine("1. " + option1);
                 Console.WriteLine("2. " + option2);
@@ -243,6 +352,80 @@ namespace HelloDungeon
 
             Console.Clear();
             return inputRecieved;
+        }
+
+        public void Run()
+        {
+            // Loop while game isn't over
+            while (gameOver == false)
+            {
+                //Print the current room to the screen
+                if (currentArea == 1)
+                {
+                    Room1();
+                }
+                if (currentArea == 2)
+                {
+                    Room2();
+                }
+                if (currentArea == 3)
+                {
+                    Room3();
+                }
+                if (currentArea == 4)
+                {
+                    Room4();
+                }
+
+                //if the player lost or beat the game
+                if (playerIsAlive == false || currentArea == 4)
+                {
+                    //...print main menu
+                    DisplayMainMenu();
+                }
+                else
+                {
+                    currentArea++;
+                }
+            }
+        }
+        float Fight( Player1 attacker, ref DungeonMaster defender)
+        {
+            float damageTaken = CalculateDamage(attacker.strength, defender.defense);
+            defender.health -= damageTaken;
+            return damageTaken;
+        }        
+        float Fight( DungeonMaster attacker, ref Player1 defender)
+        {
+            float damageTaken = CalculateDamage(attacker.strength, defender.defense);
+            defender.health -= damageTaken;
+            return damageTaken;
+        }
+        float CalculateDamage(float strength, float defense)
+        {
+            float damage = strength - defense;
+
+            //damage clamp method 1
+            if (damage <= 0)
+            {
+                damage = 0;
+            }
+
+            return damage;
+        }
+        void PrintStats(Player1 mainCharacter)
+        {
+            Console.WriteLine("Name:    " + mainCharacter.name);
+            Console.WriteLine("Health:  " + mainCharacter.health);
+            Console.WriteLine("Attack:  " + mainCharacter.strength);
+            Console.WriteLine("Defense: " + mainCharacter.defense);
+        }        
+        void PrintStats(DungeonMaster boss)
+        {
+            Console.WriteLine("Name:    " + boss.name);
+            Console.WriteLine("Health:  " + boss.health);
+            Console.WriteLine("Attack:  " + boss.strength);
+            Console.WriteLine("Defense: " + boss.defense);
         }
     }
 }
